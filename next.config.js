622c580a0dbe196930db30b45fe6d92a2a6d95f9
HigorR456 <higor.ruan456@gmail.com>
2023-03-27 20:09:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  target: 'serverless',
-  devIndicators: {
-    buildActivity: false,
-  },
 }
 
 module.exports = nextConfig
